23926f966748e6effb90ea6acf16496bff43aa16
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-02 10:35:43 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "./app.js"
+import { env } from "./env.js"
 
-const PORT = 3333
+const PORT = env.PORT
 
 app.listen(PORT, () => {
     console.log("Server is runnin on PORT ", PORT)
